19ab2917a765ac7dfdb461ba3997110a9695218c
Paul Park <ppark482@gmail.com>
2014-12-01 16:05:35 -0500

    Lists are autoupdating

--- app/scripts/tasks/tasks_controller.js
+++ app/scripts/tasks/tasks_controller.js
@@ -1,8 +1,14 @@
 (function(){
 
 	angular.module('FinalProject')
-		.controller('TasksControl', ['$scope', 'TasksFactory', '$http', 'PARSE_HEADERS', '$location', '$cookieStore',
-			function ($scope, TasksFactory, $http, PARSE_HEADERS, $location, $cookieStore) {
+		.controller('TasksControl', ['$scope', '$rootScope', 'TasksFactory', '$http', 'PARSE_HEADERS', '$location', '$cookieStore',
+			function ($scope, $rootScope, TasksFactory, $http, PARSE_HEADERS, $location, $cookieStore) {
+
+				$rootScope.$on('newTask:added', function () {
+					TasksFactory.getTasks().success( function (data) {
+					$scope.tasks = data.results;
+					});
+				});
 
 				TasksFactory.getTasks().success( function (data) {
 					$scope.tasks = data.results;
