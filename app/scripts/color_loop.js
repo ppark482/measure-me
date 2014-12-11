afdd72ad36792a1d659b64baf6d16745984162f1
Paul Park <ppark482@gmail.com>
2014-12-10 20:12:19 -0500

    Updated lists styles

---
+++ app/scripts/color_loop.js
@@ -0,0 +1,4 @@
+var colors = ['red', 'black', 'blue', 'green', 'pink'];
+var colorsCounter = 0;
+
+color = colors[colorsCounter++ % colors.length];
--- app/scripts/tasks/task_history_factory.js
+++ app/scripts/tasks/task_history_factory.js
@@ -7,14 +7,20 @@
 				var taskHistoryUrl = 'https://api.parse.com/1/classes/TasksHistory/';
 
 				var getTasks = function () {
-					var currentUser = $cookieStore.get('currentUser');
 					var currentList = $cookieStore.get('currentList');
 					var query = '?'+'where={"listId":"'+currentList.objectId+'"}';
           return $http.get(taskHistoryUrl + query, PARSE_HEADERS);
 				};
 
+				var getUserTasks = function () {
+					var currentUser = $cookieStore.get('currentUser');
+					var query = '?'+'where={"userId":"'+currentUser.objectId+'"}';
+          return $http.get(taskHistoryUrl + query, PARSE_HEADERS);
+				}
+
 				return {
-					getTasks: getTasks
+					getTasks: getTasks,
+					getUserTasks: getUserTasks
 				}
 
 			} // end function
