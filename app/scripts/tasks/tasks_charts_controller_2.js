5c613d2002550f65bf4406a602adbb8340ec98c2
Paul Park <ppark482@gmail.com>
2014-12-11 16:49:04 -0500

    Graph colors are varied

--- app/scripts/tasks/tasks_charts_controller_2.js
+++ app/scripts/tasks/tasks_charts_controller_2.js
@@ -18,18 +18,19 @@
 					var dataLabels = [];
 					var dataSets = [];
 					graphLabels = [];
-					/* Random assignment of colors */
+					/* Sequential assignment of colors */
 					var colors = ['HSLA(216, 84%, 50%, 1)', 'HSLA(7, 67%, 50%, 1)', 'HSLA(39, 91%, 66%, 1)', 'HSLA(154, 100%, 30%, 1)', 'HSLA(359, 79%, 30%, 1)'];
-					var colorsCounter = 0;
-					var color = colors[colorsCounter++ % colors.length];
+					var fillColors = ['HSLA(216, 84%, 50%, .2)', 'HSLA(7, 67%, 50%, .2)', 'HSLA(39, 91%, 66%, .2)', 'HSLA(154, 100%, 30%, .2)', 'HSLA(359, 79%, 30%, .2)'];
+					var fillColor;
+					var color;
 					// create a constructor for each individual task
 					var TaskDataSet = function (options) {
 						this.label = "Task",
-						this.fillColor = "rgba(220,220,220,0.2)",
-						this.strokeColor = "#009B57",
-						this.pointColor = color,
+						this.fillColor = options.fillColor,
+						this.strokeColor = options.pointColor,
+						this.pointColor = options.pointColor,
 						this.pointStrokeColor = "#fff",
-						this.pointHighlightFill = "#fff",
+						this.pointHighlightFill = options.pointHighlightFill,
 						this.pointHighlightStroke = "rgba(220,220,220,1)",
 						this.createdData = options.createdData,
 						this.data = options.data
@@ -42,14 +43,17 @@
 						var unique = _.uniq(taskIds);
 						var sameTasks = _.groupBy(results, 'taskId');
 						var pairsTasks = _.pairs(sameTasks);
-						console.log(pairsTasks);
+						var colorCounter = 0;
 						_.each(pairsTasks, function (y) {
 							var data = [];
 							var createdData = [];
 							var taskDataSet = new TaskDataSet({
+								pointColor : colors[colorCounter%colors.length],
+								fillColor : fillColors[colorCounter%colors.length],
 								data : data,
 								createdData : createdData
 							});
+							colorCounter++;
 							_.each(y, function (z) {
 								console.log(z);
 								// data.push(z[1]);
